ab895d02ed45e03379fd0566d045568ef9d0826c
Maria <ivanova.m.a2@edu.mirea.ru>
2023-02-23 18:57:44 +0300

    Initialisation of components from program code

--- app/src/main/java/com/example/prm_practice_2/MainActivity.java
+++ app/src/main/java/com/example/prm_practice_2/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.prm_practice_2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @SuppressLint("WrongViewCast") TextView textView = findViewById(R.id.Consent_);
+        ImageView imageView = findViewById(R.id.stars);
+        String text = getString(R.string.Consent);
+        Drawable drawable = getResources().getDrawable(R.drawable.free_icon_font_sparkles_6854012);
+        textView.setText(text);
+        imageView.setImageDrawable(drawable);
     }
 }
